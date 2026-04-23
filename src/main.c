2156d33c3b1e7922b93d8e7defaafe1f244e7094
park1525 <156728794+park1525@users.noreply.github.com>
2026-04-22 23:16:32 -0400

    joystick done

--- src/main.c
+++ src/main.c
@@ -3,23 +3,33 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void) {
+int main(void)
+{
     stdio_init_all();
     joystick_init();
+    joystick_sw_init();
 
-    while (true) {
+    while (true)
+    {
         JoystickReading joy = joystick_read();
-
         int dx = (int)joy.x - 2048;
         int dy = (int)joy.y - 2048;
 
-        // deadzone
-        if (dx > -100 && dx < 100 && dy > -100 && dy < 100) {
+        if (dx > -100 && dx < 100 && dy > -100 && dy < 100)
+        {
             printf("CENTER\n");
-        } else {
+        }
+        else
+        {
             float angle = atan2f((float)dy, (float)dx) * 180.0f / M_PI;
-            if (angle < 0) angle += 360.0f;
+            if (angle < 0)
+                angle += 360.0f;
             printf("Angle: %.1f deg\n", angle);
+        }
+
+        if (joystick_sw_consume())
+        {
+            printf("CLICK\n");
         }
 
         sleep_ms(100);
