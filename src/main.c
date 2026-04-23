7f337116d3b7fa5344fff7d9f80f6b68f03fb851
Umberto Puddu <125773480+umbertopuddu@users.noreply.github.com>
2026-04-22 20:27:44 -0400

    hello 1

--- src/main.c
+++ src/main.c
@@ -1,13 +1,13 @@
 #include "pico/stdlib.h"
 #include "config.h"
-#include "adc_joystick.h"
+// #include "adc_joystick.h"
 #include "spi_display.h"
-#include "i2c_dac.h"
-#include "pio_neopixel.h"
-#include "gpio_buttons.h"
-#include "lunar_lander.h"
-#include "car_racing.h"
-#include "tank_battle.h"
+// #include "i2c_dac.h"
+// #include "pio_neopixel.h"
+// #include "gpio_buttons.h"
+// #include "lunar_lander.h"
+// #include "car_racing.h"
+// #include "tank_battle.h"
 
 // int main(void) {
 //     stdio_init_all();
