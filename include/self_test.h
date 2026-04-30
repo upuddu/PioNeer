1995e80f780acb6d035c85d9db773429196f2433
mtuan1812 <ngmtuan1812@gmail.com>
2026-04-30 09:42:45 -0400

    branched micripython

--- include/self_test.h
+++
@@ -1,8 +0,0 @@
-#ifndef SELF_TEST_H
-#define SELF_TEST_H
-
-void led_self_test(void);
-void audio_self_test(void);
-void display_self_test(void);
-
-#endif
--- include/ui.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_MENU_H
-#define MAIN_MENU_H
-
-void main_menu_init(void);
-void main_menu_run(void);
-
-#endif
